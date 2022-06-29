d00777207378fb7fb087b0f635d98fc2958af4de
Ian Cheifetz <sylvan.iacheifetz09@gmail.com>
2022-06-29 15:18:15 -0700

    wrote out functions in albums controller and model and got test passing

--- __tests__/albums.test.js
+++ __tests__/albums.test.js
@@ -3,6 +3,7 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 const UserService = require('../lib/services/UserServices');
+const Album = require('../lib/models/Album');
 
 const mockUser = {
   username: 'Test',
@@ -26,12 +27,19 @@ describe('backend-express-template routes', () => {
     return setup(pool);
   });
   it('should be able to get all albums', async () => {
-    const [agent] = await registerAndLogin();
-    const album = await album.insert({ album_name: 'testAlbum', artist: 'testArtist', genre: 'testGenre', release_date: '1999' });
+    const [agent, user] = await registerAndLogin();
+    const album = await Album.insert({ 
+      album_name: 'testAlbum', 
+      artist: 'testArtist', 
+      genre: 'testGenre', 
+      release_date: '1999', 
+      user_id: user.id 
+    });
+    console.log(album);
     const response = await agent
       .get('/api/v1/albums');
-
-    expect(response.body).toEqual(album);
+    expect(response.status).toEqual(200);
+    expect(response.body[0]).toEqual(album);
   });
   afterAll(() => {
     pool.end();
